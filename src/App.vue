d2b060824062df1d075338c1eff474cfb13ab5a2
Kimdonghwan <kimdonghwan913@gmail.com>
2025-06-15 10:41:55 +0900

    chore: vue-composition 설정

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">Change</button>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  setup() {
+    const message = ref('hello');
+
+    function changeMessage() {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
